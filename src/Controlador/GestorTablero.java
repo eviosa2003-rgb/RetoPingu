39cafc61eaecf772d70bb7bc27224bc8727086f9
aleixmv10 <aleixmv10@alumnes.ilerna.com>
2026-04-17 15:46:48 +0200

    MAS

--- src/Controlador/GestorTablero.java
+++ src/Controlador/GestorTablero.java
@@ -1,5 +1,12 @@
 package Controlador;
 
-public class GestorTablero {
 
+public class GestorTablero {
+    public void ejecutarCasilla(Partida partida, Pinguino p, Casilla c) {
+        // TODO: implementar lógica de ejecución de la casilla
+    }
+
+    public void comprobarFinTurno(Partida partida) {
+        // TODO: implementar comprobación de fin de turno
+    }
 }
